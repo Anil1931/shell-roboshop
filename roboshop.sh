0e7209e2ac39d0787bf7c81c1f65f591d97b6cc0
Anil1931 <hanshpappa31@gmail.com>
2025-09-27 05:01:35 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0261a8d4c3a51567e"   #we have to replace with our Security group ID
+ZONE_ID="Z0061673127ECQ2F5XQ3Q" #replace with our hosted zone ID in Route 53 
+DOMAIN_NAME="hansh123.online"
 
 for instance in $@
 do 
@@ -10,9 +12,31 @@ do
     # get private IP
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"  # mongodb.hansh123.online
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME" #hansh123.online (front end URL)
+
     fi
 
     echo "$instance: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "' $IP '"
+            }]
+        }
+        }]
+    }
+    '
 done
